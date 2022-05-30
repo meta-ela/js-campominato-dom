cd6cc3f608a15682632b07fd36fc86a01ba58caa
meta-ela <anxhelameta@live.it>
2022-05-30 15:56:44 +0200

    Adds undefined numbers  correction

--- js/app.js
+++ js/app.js
@@ -16,19 +16,18 @@ cioè il numero di volte che l’utente ha cliccato su una cella che non era una
 
 
 BONUS:
-quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
-quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
-L’utente indica un livello di difficoltà in base al quale 
-viene generata una griglia di gioco quadrata, 
-in cui ogni cella contiene un numero tra quelli compresi in un range:
-- con difficoltà 1 => tra 1 e 100
-- con difficoltà 2 => tra 1 e 81
-- con difficoltà 3 => tra 1 e 49
-
-al click con il tasto destro su una cella, inseriamo il flag 
-per indicare che la cella potrebbe avere una bomba
-Il computer deve generare 16 numeri casuali - cioè le bombe - 
-compresi nello stesso range della difficoltà prescelta.
+1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+3- L’utente indica un livello di difficoltà in base al quale 
+    viene generata una griglia di gioco quadrata, 
+    in cui ogni cella contiene un numero tra quelli compresi in un range:
+    - con difficoltà 1 => tra 1 e 100
+    - con difficoltà 2 => tra 1 e 81
+    - con difficoltà 3 => tra 1 e 49
+4- al click con il tasto destro su una cella, inseriamo il flag 
+    per indicare che la cella potrebbe avere una bomba
+    Il computer deve generare 16 numeri casuali - cioè le bombe - 
+    compresi nello stesso range della difficoltà prescelta.
 
 
 con difficoltà 1 => tra 1 e 100 -------- (10 x 10)
@@ -42,53 +41,62 @@ con difficoltà 3 => tra 1 e 49 -----------(7 x 7)
 
 */
 
-// variabile globale per recuperare classe html che conterrà la griglia
+// variabili globali:
+// per recuperare classe html che conterrà la griglia
 const gridContainer = document.querySelector(".grid_container");
 
-// funzione per generare i numeri
-function generateNumbers() {
+
+
+// funzione per generare i numeri con for loop  ---> crea un loop di 100 * 100 ??????????? 
+/* function generateNumbers() {
     for(let i = 1; i <= 100; i++){
         console.log(i);
     }
 }
-generateNumbers();
+generateNumbers(); */
 
 
-// funzione per generare la griglia da 1 a 100
+// funzione per generare la griglia 
 function createGrid(xCells, yCells) {
+    // moltiplicazione per creare x e y celle per la griglia
     const cellsGrid = xCells * yCells;
     console.log(cellsGrid);
+    // var css dimensione cella * numero x
     gridContainer.style.width = `calc(var(--cell-size) * ${xCells})`;
 
-
+    // versione con dimensioni fisse
     /* const cellsGrid = 100;
     console.log(cellsGrid);
     gridContainer.style.width = `calc(var(--cell-size) * 10)`; */
 
     for (let i = 1; i <= cellsGrid; i++) {
-
-        const numberGenerator = generateNumbers();
-
+        // crea tag div a cui appendere le celle
         const cell = document.createElement("div");
-        // aggiunge alla variabile cell la classe cell con gli stili necessari
+
+        // aggiunge alla variabile cell.js la classe cell.css con gli stili necessari
         cell.classList.add("cell");
 
-        
-        cell.innerHTML = `<span>${numberGenerator}</span>`;
+        // scrive nei div dell'html il valore della variabile i 
+        cell.innerHTML = `<span>${i}</span>`;
 
+        // al click sulle celle modifico l'aspetto aggiungendo/togliendo stili css
         cell.addEventListener("click", function() {
-            // this = cell
+            // scriver this = variabile cell
             // this.innerText prende il testo all'interno 
             console.log("cliccato", this.innerText);
+            // aggiungo la classe css .clicked per le modifiche stilistiche
             this.classList.add("cell_clicked");
         })
 
+        // appende le celle create al container in html
         gridContainer.append(cell);
     }
 
-    
+
 }
 
-createGrid(10, 10);
-// createGrid(9, 9);
-// createGrid(7, 7);
+
+// le tre opzioni di griglia ma da impostare manualmente
+/* createGrid(10, 10); */
+createGrid(9, 9);
+/* createGrid(7, 7); */
